53295b2366c5514b582ca9cfa3816bc395f6f692
Ebubehope <moghaluhope@icloud.com>
2024-03-23 17:39:48 +0100

    Task5

---
+++ Module3/3A(JavaScript)/Task5.js
@@ -0,0 +1,17 @@
+const Day ='Wednesday'
+
+console.log(Day)
+
+const Wednesday =3
+
+console.log(Wednesday)
+
+const DayName =Wednesday
+
+const currentDate =3
+const dayName ='Wednesday'
+const sentence = 'Today is '+ dayName +'.' + ' it is the '+ currentDate + 'th' + ' day of the week'
+
+console.log(sentence)
+
+
